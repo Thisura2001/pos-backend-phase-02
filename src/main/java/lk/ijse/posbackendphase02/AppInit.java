0c4ce782ab0f3d5e54fbebda02bb35d0f157ce0e
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-06 16:10:27 +0530

    DB config

--- src/main/java/lk/ijse/posbackendphase02/AppInit.java
+++ src/main/java/lk/ijse/posbackendphase02/AppInit.java
@@ -1,20 +1,29 @@
 package lk.ijse.posbackendphase02;
 
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.posbackendphase02.Config.WebAppConfig;
+import lk.ijse.posbackendphase02.Config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
+    }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
     }
 }
