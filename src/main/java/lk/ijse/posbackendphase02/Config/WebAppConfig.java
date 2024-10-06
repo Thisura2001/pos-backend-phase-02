8ab053949faa50128658f201379a037c619ebe79
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-06 16:00:44 +0530

    DB config

--- src/main/java/lk/ijse/posbackendphase02/Config/WebAppConfig.java
+++ src/main/java/lk/ijse/posbackendphase02/Config/WebAppConfig.java
@@ -1,4 +1,13 @@
 package lk.ijse.posbackendphase02.Config;
 
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.posbackendphase02")
+@EnableWebMvc
+@MultipartConfig//handle the multiple file format
 public class WebAppConfig {
 }
--- src/main/java/lk/ijse/posbackendphase02/Config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/posbackendphase02/Config/WebAppRootConfig.java
@@ -1,4 +1,57 @@
 package lk.ijse.posbackendphase02.Config;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.posbackendphase02")
+@EnableJpaRepositories(basePackages = "lk.ijse.posbackendphase02.Repository")
+@EnableTransactionManagement
 public class WebAppRootConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+    @Bean
+    public DataSource dataSource() {
+        var driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/pos");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("1234");
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("lk.ijse.posbackendphase02.Entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
 }
