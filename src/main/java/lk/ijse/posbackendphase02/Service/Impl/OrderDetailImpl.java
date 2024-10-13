010905d43f2c3ce37047f8654dda6e054110519a
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-10-13 11:14:07 +0530

    orderDetail save

--- src/main/java/lk/ijse/posbackendphase02/Service/Impl/OrderDetailImpl.java
+++ src/main/java/lk/ijse/posbackendphase02/Service/Impl/OrderDetailImpl.java
@@ -1,21 +1,25 @@
 package lk.ijse.posbackendphase02.Service.Impl;
 
 import lk.ijse.posbackendphase02.Dto.Impl.OrderDetailDto;
-import lk.ijse.posbackendphase02.Dto.OrderDetailStatus;
 import lk.ijse.posbackendphase02.Repository.OrderDetailRepo;
 import lk.ijse.posbackendphase02.Service.OrderDetailService;
+import lk.ijse.posbackendphase02.Util.Mapping;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 
 @Service
 @Transactional
 public class OrderDetailImpl implements OrderDetailService {
     @Autowired
-    OrderDetailRepo orderDetailRepo;
+    private OrderDetailRepo orderDetailRepo;
+    @Autowired
+    private Mapping orderDetailMapping;
 
     @Override
     public void save(OrderDetailDto orderDetailDto) {
-
+        orderDetailRepo.save(orderDetailMapping.ToOrderDetailEntity(orderDetailDto));
     }
 }
